17ed56c270d9d9b7e372f13306d22eb8a28b66c9
chaege <chaegagim@gmail.com>
2026-07-12 23:53:32 +0900

    chore: base.css 색깔 변수로 변경, 다크모드 했던거 삭제 , 헤더부분 header.css 로 링크연결,+ 내용 이동 , 카드 패딩 수정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 import { initBoard } from './ui/board.js';
 import { initModal } from './ui/modal.js';
 import { initControls } from './ui/controls.js';
-import { initHeader, initThemeToggle } from './ui/header.js';
+import { initHeader } from './ui/header.js';
 import { initStats } from './ui/stats.js';
 
 initBoard();
 initModal();
 initControls();
 initHeader();
-initThemeToggle();
 initStats();
